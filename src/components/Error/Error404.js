c92f0895e34490e44ac08d91a44bbce6e5083c54
Valeria Silveira <vsol.sil@gmail.com>
2023-11-06 20:19:36 -0300

    remove lottie

--- src/components/Error/Error404.js
+++ src/components/Error/Error404.js
@@ -1,39 +1,32 @@
-import React, { useContext } from 'react'
-import { Button } from 'antd'
-import { Link } from 'react-router-dom'
-import { UIContext } from '../Context/UIContext'
-import Lottie from 'react-lottie'
-import error404Json from './error404Json.json'
+import React, { useContext } from "react";
+import { Button } from "antd";
+import { Link } from "react-router-dom";
+import { UIContext } from "../Context/UIContext";
 
 const Error404 = () => {
-    const { darkMode } = useContext(UIContext)
+  const { darkMode } = useContext(UIContext);
 
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: error404Json
-    }
+  return (
+    <div className="container">
+      <div className="error">
+        <h2 className={darkMode ? "dark-text" : ""}>
+          Lo sentimos... parece que esta página no existe
+        </h2>
 
-    return (
-        <div className="container">
-            <div className="error">
+        <Link to="/">
+          <Button
+            className={
+              darkMode ? "button cart-button dark-button" : "button cart-button"
+            }
+            type="primary"
+            shape="round"
+          >
+            Volver al inicio
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-                <h2 className={darkMode ? 'dark-text' : ''}>Lo sentimos... parece que esta página no existe</h2>
-                <Lottie options={defaultOptions} />
-
-                <Link to="/" >
-                    <Button 
-                        className={darkMode ? 'button cart-button dark-button' : 'button cart-button'}
-                        type="primary"
-                        shape="round"
-                    >
-                            Volver al inicio
-                    </Button>
-                </Link>
-                
-            </div>
-        </div>
-    )
-}
-
-export default Error404
+export default Error404;
